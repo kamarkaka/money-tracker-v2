7e3cce091a97db0ec01ffd773fe82e798d90e233
Meng Cao <mengcao@meta.com>
2026-03-23 21:17:22 -0400

    finalize app before publish

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -2,33 +2,20 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "Money Tracker",
-  slug: "money-tracker",
+  name: "Money Tracker 2",
+  slug: "money-tracker-2",
   version: "1.0.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
-  scheme: "money-tracker",
-  splash: {
-    image: "./assets/splash-icon.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
-  },
+  scheme: "money-tracker-2",
   ios: {
     supportsTablet: true,
-    bundleIdentifier: "com.moneytracker.app",
-  },
-  android: {
-    adaptiveIcon: {
-      backgroundColor: "#E6F4FE",
-      foregroundImage: "./assets/android-icon-foreground.png",
-      backgroundImage: "./assets/android-icon-background.png",
-      monochromeImage: "./assets/android-icon-monochrome.png",
+    bundleIdentifier: "xyz.mengcao.money-tracker-2",
+    buildNumber: "1",
+    infoPlist: {
+      NSPhotoLibraryUsageDescription: "Used to export financial data files.",
     },
-    package: "com.moneytracker.app",
   },
   plugins: ["expo-router", "expo-secure-store", "expo-sqlite", "expo-document-picker"],
-  extra: {
-    apiUrl: process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000",
-  },
 });
