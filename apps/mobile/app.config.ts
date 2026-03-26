81f27017ef71059a488eca70e65ffcdd7286a953
Meng Cao <mengcao@meta.com>
2026-03-26 08:36:34 -0400

    update to version 2.0.0

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Money Tracker 2",
   slug: "money-tracker-2",
-  version: "1.0.0",
+  version: "2.0.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
